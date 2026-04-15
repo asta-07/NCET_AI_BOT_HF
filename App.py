dbcf53885304d07c1850a0249e7d227220f32c83
asta-07 <astab9450@gmail.com>
2026-04-15 04:56:11 -0400

    Update App.py

--- App.py
+++ App.py
@@ -1,1 +1,10 @@
+import stream as st
+from transformers import pipeline
+@st.cache_resource
+def load_summerizer():
+    return pipeline("summarization", model="sshleifer/distilbart-cnn-12-6")
 
+summarizer = load_summarizer()
+
+st.title(" AI text Summarizer")
+st.write("Enter a long text below, and get a concise summary!")
